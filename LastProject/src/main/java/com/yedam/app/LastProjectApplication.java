1a0ffb80267eea3f5f63b0c90e337abbff6338df
rlaxodus <hbj04003@gmail.com>
2023-07-06 17:32:36 +0900

    23/07/06 17:32

--- LastProject/src/main/java/com/yedam/app/LastProjectApplication.java
+++ LastProject/src/main/java/com/yedam/app/LastProjectApplication.java
@@ -15,13 +15,4 @@ public class LastProjectApplication {
 		SpringApplication.run(LastProjectApplication.class, args);
 	}
 	
-	@GetMapping("main")
-	public String main() {
-		return "main/layout";
-	}
-	
-	@GetMapping("main2")
-	public String main2() {
-		return "domain/stock";
-	}
 }
