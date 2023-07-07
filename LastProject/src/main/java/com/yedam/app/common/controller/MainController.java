647bddaa62b019ceb1bed07a3125784291025c2b
tsd02150 <tsd02150@naver.com>
2023-07-07 15:41:28 +0900

    layout move

--- LastProject/src/main/java/com/yedam/app/common/controller/MainController.java
+++ LastProject/src/main/java/com/yedam/app/common/controller/MainController.java
@@ -6,8 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 	
-	
-	@GetMapping("main")
+	@GetMapping("/")
 	public String mainPage() {
 		return "main/main";
 	}
