9f3f4915db1d994438c616ccf06a34ccd8caed4a
S'sisters <S'sisters@DESKTOP-ER5I7KS>
2023-07-08 17:37:35 +0900

    230708

---
+++ LastProject/src/main/java/com/yedam/app/mall/controller/MallController.java
@@ -0,0 +1,46 @@
+package com.yedam.app.mall.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import com.yedam.app.mall.service.MallService;
+import com.yedam.app.mall.service.ProductVO;
+
+@Controller
+@RequestMapping("mall")
+public class MallController {
+	
+	@Autowired
+	MallService mallService;
+	
+	// 포인트몰 메인 페이지
+	@GetMapping("mallMain")
+	public String mallMain(Model model) {
+		List<ProductVO> list = mallService.getProductList();
+		model.addAttribute("product", list);
+		System.out.println(list);
+		return "mall/mallMain";
+	}
+	
+	// 농산물 페이지
+	@GetMapping("farmList")
+	public String farmList(Model model) {
+		List<ProductVO> farmList = mallService.getProductList();
+		model.addAttribute("farmList", farmList);
+		return "mall/farmList";
+	}
+
+	// 수산물 페이지
+	@GetMapping("seafoodList")
+	public String seafoodList(Model model) {
+		List<ProductVO> seafoodList = mallService.getProductList();
+		model.addAttribute("seafoodList", seafoodList);
+		return "mall/seafoodList";
+	}
+}
---
+++ LastProject/src/main/java/com/yedam/app/mall/service/ProductVO.java
@@ -0,0 +1,25 @@
+package com.yedam.app.mall.service;
+
+import java.util.Date;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+@Data
+public class ProductVO {
+	
+	private String prdtNo;
+	private String nm;
+	private int prc;
+	private int dcRate;
+	private String desct;
+	private String thumb;
+	private String img;
+	private int stcCnt;
+	@DateTimeFormat(pattern="yyyy-MM-dd")
+	private Date drwupDt;
+	private String commonCd;
+		
+
+}
