b575f192c4255bae798ff4f6703c34e98f223562
kmhyang <ihyang00@naver.com>
2023-07-13 17:47:11 +0900

    230713 mypage

--- LastProject/src/main/java/com/yedam/app/security/CustomFailureHandler.java
+++ LastProject/src/main/java/com/yedam/app/security/CustomFailureHandler.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
@@ -15,8 +16,9 @@ public class CustomFailureHandler implements AuthenticationFailureHandler{
    @Override
    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
          AuthenticationException exception) throws IOException, ServletException {
-	   
-      System.out.println("custom Fail");
+	   System.out.println("custom Fail");
+	   HttpSession session = request.getSession();
+	   session.setAttribute("message", "다시 시도해주세요");
       response.sendRedirect("/member/login");
    }
    
