dc23d837e29aeffd5ce8337ec7ced02a9e310c8c
tsd02150 <tsd02150@naver.com>
2023-07-18 17:51:01 +0900

    2023-07-18 익명채팅방 포맷 완

--- LastProject/src/main/java/com/yedam/app/community/controller/ChatController.java
+++ LastProject/src/main/java/com/yedam/app/community/controller/ChatController.java
@@ -2,11 +2,21 @@ package com.yedam.app.community.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.yedam.app.community.service.ChatService;
 
 @Controller
+@RequestMapping("community")
 public class ChatController {
 	@Autowired
 	ChatService chatService;
+	
+	@GetMapping("chat")
+	public String chat() {
+		
+		
+		return "community/chat";
+	}
 }
