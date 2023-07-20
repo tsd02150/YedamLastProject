19694d4bf6b822be601e9ceb50d00defef804d5e
hbj04003 <hbj04003@DESKTOP-5PG3J38>
2023-07-20 09:37:31 +0900

    2023/07/20 실시간알림중 taeyeon

--- LastProject/src/main/java/com/yedam/app/stock/controller/StockSocketConfig.java
+++ LastProject/src/main/java/com/yedam/app/stock/controller/StockSocketConfig.java
@@ -1,4 +1,4 @@
-package com.yedam.app.stock.controller;
+ package com.yedam.app.stock.controller;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
---
+++ LastProject/src/main/java/com/yedam/app/stock/service/impl/StockSchedule.java
@@ -0,0 +1,7 @@
+package com.yedam.app.stock.service.impl;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StockSchedule {
+}
