c50bf20cfbb9aa71471e710da71be81bad3f5321
S'sisters <S'sisters@DESKTOP-ER5I7KS>
2023-07-24 03:38:31 +0900

    20230723-장바구니, 주문내역 아직도 작업중

--- LastProject/src/main/java/com/yedam/app/mall/service/OrderVO.java
+++ LastProject/src/main/java/com/yedam/app/mall/service/OrderVO.java
@@ -6,28 +6,38 @@ import org.springframework.format.annotation.DateTimeFormat;
 
 import lombok.Data;
 
-
 @Data
 public class OrderVO {
-	
+
 	private String orderNo;
 	private String membNo;
-	@DateTimeFormat(pattern="yyyy-MM-dd")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
 	private Date orderDt;
 	private int dcRate;
 	private String shpcst;
 	private String orderAm;
 	private String orderSt;
 	private int payAn;
-	
+
 	private String prdt_no;
 	private String id;
-	
+
 	private String orderDetaNo;
 	private String prdtNo;
 	private int cnt;
+	private String nm;
+	private int point;
+	private String thumb;
 	
+	// 주문 필요정보
+	private String tel;
+	private String mobile;
+	private String addrNo;
+	private String zip;
+	private String addr;
+	private String detaAddr;
+
 	// 정렬
 	private String order;
-	
+
 }
