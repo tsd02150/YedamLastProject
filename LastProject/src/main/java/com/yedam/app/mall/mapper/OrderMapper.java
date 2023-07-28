e262d11b610791b47859fe004284ad1fc230026b
user <user@DESKTOP-81AIGNU>
2023-07-28 16:27:51 +0900

    임시저장

--- LastProject/src/main/java/com/yedam/app/mall/mapper/OrderMapper.java
+++ LastProject/src/main/java/com/yedam/app/mall/mapper/OrderMapper.java
@@ -3,11 +3,13 @@ package com.yedam.app.mall.mapper;
 import java.util.List;
 
 import com.yedam.app.mall.service.OrderVO;
+import com.yedam.app.member.service.MembVO;
 
 public interface OrderMapper {
 	
 	//주문 리스트
 	public List<OrderVO> getOrderList(String membNo);
 	public List<OrderVO> getMemInfo(String membNo);
+	public int insertOrder(OrderVO ordVO); 
 
 }
